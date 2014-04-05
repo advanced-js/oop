347965c3243b58f72cb5b2f7c4e8bb30f2aa20b7
Aidan Feldman <afeld@github.com>
2014-04-05 03:07:44 -0400

    add jshint to grunt task

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,14 +1,22 @@
 module.exports = function(grunt) {
   grunt.initConfig({
+    jshint: {
+      files: {
+        src: ['*.js']
+      }
+    },
     validation: {
       options: {
         reset: true
       },
-      files: '*.html'
+      files: {
+        src: ['*.html']
+      }
     }
   });
 
+  grunt.loadNpmTasks('grunt-contrib-jshint');
   grunt.loadNpmTasks('grunt-html-validation');
 
-  grunt.registerTask('default', ['validation']);
+  grunt.registerTask('default', ['jshint', 'validation']);
 };
