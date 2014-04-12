0e234032709afdf5b4300802c523d126c66595de
Ross Christie <rosspchristie@gmail.com>
2014-04-12 12:23:09 -0400

    circle exercise

--- circle.js
+++ circle.js
@@ -1,1 +1,11 @@
-// var Circle = ...
+var Circle = function(radius){ 
+ this.radius = radius; 
+ this.getArea = function(){ 
+   return radius * radius * Math.PI;
+   }; 
+}; 
+
+var circle = new Circle(1); 
+circle.getArea();
+
+//console.log(circle.getArea());
--- tests.js
+++ tests.js
@@ -1,5 +1,7 @@
-test("hello test", function() {
-  ok(1 + 1 === 2, "Passed!");
+test("circle test", function() {
+  var circle = new Circle(1);
+  deepEqual(circle.radius, 1);
+  ok( circle.getArea() === 3.141592653589793 );
 });
 
 // ADD TESTS HERE
