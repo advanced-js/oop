92ece51f55d44d66d57eef96659a23fe1f3cfe26
James Rothpearl <jrothpearl@gmail.com>
2014-04-12 12:23:58 -0400

    I need to include library support for 'close' test method, otherwise tested for circle radius

--- tests.js
+++ tests.js
@@ -2,4 +2,15 @@ test("hello test", function() {
   ok(1 + 1 === 2, "Passed!");
 });
 
-// ADD TESTS HERE
+test("string length", function() {
+  var str = "abc";
+  ok(str.length === 3, "is the correct length");
+});
+
+// ADD TESTS HERE FOR CIRCLE CLASS
+
+test("circle test", function() {
+var circle1 = new Circle(5);
+deepEqual(circle1.radius, 5, "Got the radius");
+// deepEqual(circle1.getArea(), Math.PI * 25, "Area method works, returns same value");
+});
