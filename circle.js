6865a542bf5c3d2b75cb507c0ab58efbaf13fbeb
Damian Kowal <damian@damiankowal.com>
2014-04-12 12:05:56 -0400

    Add area property and additional tests; Circle class appears to be working correctly

--- circle.js
+++ circle.js
@@ -1,4 +1,7 @@
 var Circle = function(radius) {
     this.radius = radius;
+    this.area = function () {
+        return Math.PI * Math.pow( this.radius, 2 );
+    }
 };
 
--- tests.js
+++ tests.js
@@ -13,11 +13,21 @@ test("radius property", function() {
     ok( circ2.radius === 2 )
 })
 
-test("area method", function() {
+test("area method exists", function() {
     var radius = 2;
     var circ2 = new Circle( radius );
     ok( circ2.area );
-    ok( circ2.area() === Math.PI * Math.pow( radius, 2 ) );
 })
 
+test("area method works", function() {
+    var radius1 = 2,
+        radius2 = 400,
+        radius3 = .001324,
+        circ1 = new Circle( radius1 ),
+        circ2 = new Circle( radius2 ),
+        circ3 = new Circle( radius3 );
+    ok( circ1.area() === Math.PI * Math.pow( radius1, 2 ) );
+    ok( circ2.area() === Math.PI * Math.pow( radius2, 2 ) );
+    ok( circ3.area() === Math.PI * Math.pow( radius3, 2 ) );
+})
 
