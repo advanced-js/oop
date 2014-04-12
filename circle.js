215786b88f6d287306c06e8e6a7d39d6e4a52a9f
juliangibellino <jgibelli@172-30-28-51.dynapool.nyu.edu>
2014-04-12 12:16:07 -0400

    Created Circle class and unit testing

--- circle.js
+++ circle.js
@@ -1,1 +1,8 @@
-// var Circle = ...
+var Circle = function(radius){
+	this.radius = radius;
+	this.getArea = function(){
+		return (3.14 * Math.pow(this.radius, 2));
+	}
+}
+
+var myCircle = new Circle(20);
--- tests.js
+++ tests.js
@@ -1,5 +1,16 @@
-test("hello test", function() {
-  ok(1 + 1 === 2, "Passed!");
+//Test Class and its properties
+test("test Class", function() {
+	//Test the area method
+	function areaTest(then){
+		deepEqual(then.getArea(), (3.14 * Math.pow(then.radius, 2)), "Area passed" );
+	};
+	
+	areaTest(new Circle(50));
 });
 
-// ADD TESTS HERE
+//Test and instance of the Circle class and its area
+test("test area of instance", function() {
+	var circleTest = new Circle(50);
+	deepEqual(circleTest.getArea(), (3.14 * Math.pow(circleTest.radius, 2)), "Area passed" );
+});
+
