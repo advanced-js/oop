9503c531f838d534fa8421f52545ace9757a83c2
Zena Hirsch <hirsch.zena@gmail.com>
2014-06-07 20:13:49 -0400

    Define Superhero class and methods, and create unit tests

--- js/super_hero.js
+++ js/super_hero.js
@@ -1,1 +1,85 @@
-// var SuperHero = ...
+var states = ['neutral', 'attacking', 'defending', 'resting', 'dead'];
+
+var SuperHero = function (params) {
+	this.name = params.name;
+
+	/* MODIFIERS */
+	this.power = params.power; // attack power (out of 10)
+	this.defense = params.defense; // defense power (out of 10)
+	this.tempDefense = params.defense + 10;
+
+	/* CONSUMABLES */
+	this.toughness = params.toughness; // health points (out of 200)
+	this.strength = params.strength; // strength points (out of 20)
+};
+
+// Superhero's present state
+// 0: Neutral
+// 1: Attacking
+// 2: Defending
+// 3: Resting
+// 4: Dead
+SuperHero.prototype.state = 0;
+
+SuperHero.prototype.attack = function (superhero) {
+	if (this.strength > 1) {
+		this.state = 1;
+		superhero.toughness -= (2 * this.power) + superhero.getDefense();
+		this.strength -= 2;
+
+		if (superhero.toughness <= 0) {
+			superhero.state = 4;
+			console.log(this.name + ' killed ' + superhero.name + '!');
+		}
+
+		return true;
+
+	} else {
+		console.log(this.name + ' doesn\'t have enough strength left!');
+		return false;
+	}
+};
+
+SuperHero.prototype.getStrengthBoost = function () {
+	this.strength += 10;
+	return true;
+};
+
+SuperHero.prototype.getDefense = function () {
+	if (this.state === 2) {
+		return this.defense + 10;
+	} else {
+		return this.defense;
+	}
+};
+
+SuperHero.prototype.defend = function () {
+	this.state = 2;
+	return true;
+};
+
+SuperHero.prototype.rest = function () {
+	this.state = 3;
+	this.toughness += 5;
+	return true;
+};
+
+SuperHero.prototype.highFive = function (superhero) {
+	if (this.state !== 4 && superhero.state !== 4) {
+		this.power += 1;
+		this.defense += 1;
+		this.toughness += 10;
+		this.strength += 4;
+
+		superhero.power += 1;
+		superhero.defense += 1;
+		superhero.toughness += 10;
+		superhero.strength += 4;
+
+		return true;
+
+	} else {
+
+		return false;
+	}
+};
--- js/tests.js
+++ js/tests.js
@@ -1,5 +1,133 @@
-test("hello test", function() {
-  ok(1 + 1 === 2, "Passed!");
+function getNewSuperman (toughness, strength) {
+	return new SuperHero ({
+		name: 'Superman',
+		power: 10,
+		defense: 8,
+		toughness: toughness || 150,
+		strength: strength || 20
+	});
+}
+
+function getNewBatman (toughness, strength) {
+	return new SuperHero ({
+		name: 'Batman',
+		power: 8,
+		defense: 9,
+		toughness: toughness || 170,
+		strength: strength || 20
+	});	
+};
+
+test('Superman initialization', function() {
+	var s = getNewSuperman();
+
+	ok(s.name === 'Superman', 'Superman\'s name initialized to: ' + s.name);
+	ok(s.power === 10, 'Superman\'s power initialized to: ' + s.power);
+	ok(s.defense === 8, 'Superman\'s defense initialized to: ' + s.defense);
+	ok(s.toughness === 150, 'Superman\'s toughness initialized to: ' + s.toughness);
+	ok(s.strength === 20, 'Superman\'s strength initialized to: ' + s.strength);
+	ok(s.state === 0, 'Superman\'s state initialized to: ' + states[s.state]);
+	
 });
 
-// ADD TESTS HERE
+test('Superman attacks Batman', function() {
+	var s = getNewSuperman();
+	var b = getNewBatman();
+
+	ok(s.state === 0, 'Superman is not attacking, state is: ' + states[s.state]);
+
+	ok(s.attack(b) === true, 'Superman attacks Batman!');
+	ok(s.state === 1, 'Superman\'s state set correctly to: ' + states[s.state]);
+
+	// Batman toughness: 170 - ((2*10) + 9) = 141
+	ok(b.toughness === 141, 'Batman\'s toughness reduced correctly: ' + b.toughness);
+
+	// Superman's strength: 20 - 2 = 18
+	ok(s.strength === 18, 'Superman\'s strength reduced correctly: ' + s.strength);
+});
+
+test('Batman does not have sufficient strength to attack', function() {
+	var s = getNewSuperman();
+	var b = getNewBatman(20, 1);
+
+	ok(b.strength === 1, 'Batman\'s strength is: ' + b.strength);
+	ok(b.attack(s) === false, 'Batman cannot attack, does not have sufficient strength');
+});
+
+
+test('Batman defends', function () {
+	var s = getNewSuperman();
+	var b = getNewBatman();
+
+	ok(b.state === 0, 'Batman is not defending, state is: ' + states[b.state]);
+	ok(b.getDefense() === 9, 'Batman\'s defense is: ' + b.getDefense());
+	ok(b.defend() === true, 'Batman defends');
+	ok(b.state === 2, 'Batman\'s state set to: ' + states[b.state]);
+
+	// Batman's tempDefense: 9 + 10 = 19
+	ok(b.getDefense() === 19, 'Batman\'s defense increased correctly to: ' + b.getDefense());
+});
+
+test('Superman rests', function () {
+	var s = getNewSuperman();
+	var b = getNewBatman();
+
+	ok(s.toughness === 150, 'Superman\'s toughness is: ' + 150);
+	ok(s.rest() === true, 'Superman rests');
+	ok(s.state === 3, 'Superman\'s state set to: ' + states[s.state]);
+
+	// Superman toughness: 150 + 5 = 155
+	ok(s.toughness === 155, 'Superman\'s toughness increased correctly to: ' + s.toughness);
+});
+
+test('Batman gets a strength boost', function () {
+	var s = getNewSuperman();
+	var b = getNewBatman();
+
+	ok(b.strength === 20, 'Batman\'s strength is: ' + b.strength);
+	ok(b.getStrengthBoost() === true, 'Batman gets a strength boost');
+
+	// Batman strength boost: 20 + 10 = 30
+	ok(b.strength === 30, 'Batman\'s strength is: ' + b.strength);
+});
+
+test('Batman kills Superman', function () {
+	var s = getNewSuperman(1); // reduce base toughness for purposes of test
+	var b = getNewBatman();
+
+	ok(s.state === 0, 'Superman\'s base state is: ' + states[s.state]);
+	ok(s.toughness === 1, 'Superman\'s toughness is: ' + s.toughness);
+	ok(b.attack(s) === true, 'Batman attacks Superman');
+
+	// Superman's toughness: 1 - 24 = -23
+	ok(s.toughness === -23, 'Superman\'s toughness is now: ' + s.toughness);
+
+	ok(s.state === 4, 'Superman\'s toughness is <0, Superman\'s state is now: ' + states[s.state]);
+});
+
+test('Superheros high five (both alive)', function () {
+	var s = getNewSuperman();
+	var b = getNewBatman();
+
+	ok(s.highFive(b) === true, 'Superman and Batman high five!');
+
+	ok(s.power === 11, 'Superman power increased by 1: ' + s.power); // 10 + 1 = 11
+	ok(s.defense === 9, 'Superman defense increased by 1: ' + s.defense); // 8 + 1 = 9
+	ok(s.toughness === 160, 'Superman toughness increased by 10: ' + s.toughness); // 150 + 10 = 160
+	ok(s.strength === 24, 'Superman strength increased by 4: ' + s.strength); // 20 + 4 = 24
+
+	ok(b.power === 9, 'Batman power increased by 1: ' + b.power); // 8 + 1 = 9
+	ok(b.defense === 10, 'Batman defense increased by 1: ' + b.defense); // 9 + 1 = 10
+	ok(b.toughness === 180, 'Batman toughness increased by 10: ' + b.toughness); // 170 + 10 = 180
+	ok(b.strength === 24, 'Batman strength increased by 4: ' + b.strength); // 20 + 4 = 24
+});
+
+test('Superheros high five (one dead)', function () {
+	var s = getNewSuperman();
+	var b = getNewBatman(1); // reduce base toughness for purposes of test
+
+	s.attack(b);
+
+	ok(b.state === 4, 'Superman attacks Batman, Batman\'s state is now: ' + states[b.state]);
+	ok(s.highFive(b) === false, 'Superman and Batman cannot high five because Batman is dead. Bummer.');
+});
