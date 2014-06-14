dc72e659bf1302e0b74aba41a078f7244b492757
mroyt <michael.roytburd@gmail.com>
2014-06-13 21:15:35 -0400

    Pretty basic start
    i use the timer to refresh stamina and such, and im having trouble
    figuring out how to test for that

--- js/super_hero.js
+++ js/super_hero.js
@@ -1,1 +1,60 @@
-// var SuperHero = ...
+
+var SuperHero = function(name,heightInCms,weightInKgs,strength,toughness,speed,stamina){
+	this.name = name;
+	this.height = heightInCms;
+	this.weight = weightInKgs;
+	this.strength = strength;
+	this.toughness = toughness;
+	this.speed = speed;
+	this.stamina = stamina;
+	this.level = 1;
+	this.expPoints = 0;
+	
+	//doubles toughness and halves strength for 1 minute. 
+	this.fortify = function(){
+		var startStrength = this.strength;
+		var startToughness = this.toughness;
+		this.strength = startStrength/2;
+		this.toughness = startToughness*2;
+		//this.reduceStamina(10);
+		this.incExpPoints(2);
+
+			console.log("new strength is: "+this.strength);console.log("new toughness is: "+this.toughness);		
+		setTimeout(function(){
+			this.strength = startStrength;
+			this.toughness = startToughness;
+				console.log("new strength is: "+this.strength);console.log("new toughness is: "+this.toughness);		
+		},6000);
+		
+	};
+
+	//Level up when called, up the stats, and empty exp points
+	this.levelUp = function(){
+		this.level += 1;
+		this.expPoints = 0;
+		this.strength += this.level;
+		this.toughness += this.level;
+		this.speed += this.level;
+		this.stamina += this.level;
+
+	};
+	//Increment experience points by arg
+	this.incExpPoints = function(r){
+		this.expPoints += r;
+	}
+	//Decerement stamina by arg, and restore it by 1 point every 5 seconds
+	this.reduceStamina = function(r){
+		var startingStamina = this.stamina;
+		this.stamina -= r;
+		console.log("stamina is reduced to: "+this.stamina);
+		var interval = setInterval(function(){
+				this.stamina++;
+				console.log("stamina is: "+this.stamina);
+				console.log("startingstamina is: "+startingStamina);
+				if (this.stamina===startingStamina){
+					clearInterval(interval);
+				}
+			},5000);
+		};
+	};
+
--- js/tests.js
+++ js/tests.js
@@ -1,5 +1,36 @@
-test("hello test", function() {
-  ok(1 + 1 === 2, "Passed!");
+//var SuperHero = function(name,heightInCms,weightInKgs,strength,toughness,speed,stamina)
+var stretch = new SuperHero('Mr. Stretch',220,90,40,90,100,110);
+var brick = new SuperHero('Brickhouse',190,150,120,100,20,50);
+
+test("Test Mr. Stretch",function(){
+//test the basic properties	
+	ok(stretch.name==='Mr. Stretch',"Yep, his name is Mr. Stretch");
+	ok(stretch.height===220,"Yep, "+stretch.name+" is 220cm tall");
+	ok(stretch.weight===90,"Yep, "+stretch.name+" weighs 90 kilos");
+	ok(stretch.strength===40,"Yep, "+stretch.name+" has strength of 40");
+	ok(stretch.toughness===90,"Yep, "+stretch.name+" has toughness of 90");
+	ok(stretch.speed===100,"Yep, "+stretch.name+" has speed of 100");
+	ok(stretch.stamina===110,"Yep, "+stretch.name+" has stamina of 110");
 });
 
-// ADD TESTS HERE
+test("Test the fortify power",function(){
+//test fortify
+	stretch.fortify();
+	ok(stretch.strength===20,stretch.name+"'s strength has been halved!")
+	ok(stretch.toughness===180,stretch.name+"'s toughness has been doubled!")
+	ok(stretch.expPoints===2,stretch.name+"'s experience points went up by 2!")
+	//can't get the one below to work
+	//ok(stretch.stamina===100,stretch.name+"'s stamina went down by 10 points!")
+	console.log("Waiting for stats to return to normal");
+	/*setTimeout(function(){
+		ok(stretch.stamina===102,stretch.name+"'s stamina went up 2 points in 10 seconds")
+	},1000);
+	setTimeout(function(){
+		ok(stretch.stamina===110,stretch.name+"'s stamina is back to normal")
+	},4000);*/
+});
+setTimeout(function(){
+	test("Test restore after fortify",function(){
+		ok(stretch.strength===40,stretch.name+" is back to full strength!")
+		ok(stretch.toughness===90,stretch.name+"'s toughness is back to normal!")
+	}),10000});
