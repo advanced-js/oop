b2be41b1fe933d2037342fc74823488013b4e252
Justine Tiu <justine.r.tiu@gmail.com>
2014-06-21 11:43:27 -0400

    Superhero with subclasses

--- js/super_hero.js
+++ js/super_hero.js
@@ -2,7 +2,7 @@
 var SuperHero = function(name) {
 	this.name = name;
 	
-	this.health = 100;  //why doesn't SuperHero.prototype.health = function() { return 100; } work?
+	this.health = 100;  
 	this.defense = 10;
 	this.strength = 10;
 	this.speed = 10;
@@ -24,11 +24,82 @@ SuperHero.prototype.smile = function(enemy) {
 SuperHero.prototype.punch = function(enemy) {
 	//when the enemy's defense is lowered, the punch has more impact
 	var multiplier = (10 - enemy.defense)*(this.strength);
-	this.strength = multiplier;
-	enemy.health = enemy.health - multiplier;
+	if (multiplier != 0) {
+		enemy.health = enemy.health - multiplier;
+	}
+	else {
+		enemy.health = enemy.health - this.strength;
+	}
 }
 
 //hugging adds 10HP to the "enemy"
 SuperHero.prototype.hug = function(enemy) {
 	enemy.health += 10;
+}
+
+//kid subclass
+var kidHero = function(name){
+	this.name = name;
+	this.strength = 5;
+	this.health = 40;
+};
+
+kidHero.prototype = new SuperHero();
+
+//kids can eat anything to gain health
+kidHero.prototype.eat = function() {
+	this.health += 10;
+	return this.health;
+}
+
+//but kids hurt themselves a bit when they punch
+kidHero.prototype.punch = function(enemy) {
+	//when the enemy's defense is lowered, the punch has more impact
+	var multiplier = (10 - enemy.defense)*(this.strength);
+	if (multiplier != 0) {
+		enemy.health = enemy.health - multiplier;
+	}
+	else {
+		enemy.health = enemy.health - this.strength;
+	}
+	this.health -= 1;
+}
+
+//shapeshifters have a random combination of strength, defense, and speed
+var shapeshifter = function(name, combo) {
+	this.name = name;
+	
+	if (combo === 1) {
+		this.strength = 3;
+		this.defense = 8;
+		this.speed = 10;
+	}
+	else if (combo === 2) {
+		this.strength = 9;
+		this.defense = 7;
+		this.speed = 5;
+	}
+	else if (combo == 3) {
+		this.strength = 5;
+		this.defense = 10;
+		this.speed = 4;
+	}
+	else {
+		this.strength = 4;
+		this.defense = 4;
+		this.speed = 4;
+	}
+	
+};
+shapeshifter.prototype = new kidHero();
+
+//shapeshifters can also absorb their enemy's traits but it takes 2 seconds.  It also hurts the enemy.
+shapeshifter.prototype.absorb = function(enemy) {
+	var that = this; //this is how you hold onto the shapeshifter context
+	setTimeout(function() {
+		enemy.health -= 5;
+		that.strength = enemy.strength;
+		that.defense = enemy.defense;
+		that.speed = enemy.speed;
+	}, 2000);
 }
--- js/tests.js
+++ js/tests.js
@@ -50,3 +50,47 @@ test("Elastigirl regrets hurting Mr. Incredible and gives him a hug", function()
 	elastigirl.hug(incredible);
 	equal(incredible.health, 90, "Mr. Incredible regains 10HP!");
 });
+
+//kid hero tests
+test("Dash and Violet are ready to rumble", function() {
+	var dash = new kidHero("Dash");
+	var violet = new kidHero("Violet");
+	equal(dash.name, "Dash", "Meet Dash");
+	equal(dash.health, 40, "Full health");
+	equal(dash.strength, 5, "Full strength");
+	equal(violet.name, "Violet", "Meet Violet");
+	equal(violet.health, 40, "Full health");
+	equal(violet.strength, 5, "Full strength");
+});
+
+test("Dash eats his sister's milkshake and she punches him in retaliation", function() {
+	var dash = new kidHero("Dash");
+	var violet = new kidHero("Violet");
+	dash.eat();
+	equal(dash.health, 50, "Dash eats a milkshake and gains 10 HP");
+	violet.punch(dash);
+	equal(dash.health, 45, "Dash loses 5 HP");
+	equal(violet.health, 39, "Violet loses 1 HP");
+});
+
+test("Jack Jack is ready to rumble", function() {
+	var jackJack = new shapeshifter("Jack Jack", 2);
+	equal(jackJack.name, "Jack Jack", "Meet Jack Jack");
+	equal(jackJack.strength, 9, "Jack Jack has 9 strength");
+	equal(jackJack.defense, 7, "7 defense");
+	equal(jackJack.speed, 5, "5 speed");
+});
+
+asyncTest("Jack Jack absorbs Violet's traits, hurting her and helping him in the process", function() {
+	var jackJack = new shapeshifter("Jack Jack", 2);
+	var violet = new kidHero("Violet");
+	equal(violet.health, 40, "Violet starts at 40 HP");
+	jackJack.absorb(violet);
+	setTimeout(function() { 	
+    	equal(jackJack.strength, 5, "after 2 sec, Jack Jack has 5 strength");
+		equal(jackJack.defense, 10, "10 defense");
+		equal(jackJack.speed, 10, "10 speed");
+		equal(violet.health, 35, "after 2 sec, Violet's loses 5 HP");
+		start();
+  	}, 2500);
+});
