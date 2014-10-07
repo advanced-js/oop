62989e9433e0953d0fe3f8208cd100de9047bea7
DJ Rosenbaum <dave@djrconcepts.com>
2014-10-07 12:30:16 -0400

    Guardians of the Galaxy SuperHero Class with Qunit testing

--- js/super_hero.js
+++ js/super_hero.js
@@ -1,1 +1,27 @@
-// var SuperHero = ...
+//DEFINE SuperHero Class
+function SuperHero( hero_name, strength ) {
+  this.hero_name = hero_name;
+  this.strength = strength;
+  this.health = 100;
+}
+
+//Add an attack function to the SuperHero Class
+SuperHero.prototype.attack = function( defender ) {
+  console.log( this.hero_name + ' attacked ' + defender.hero_name + ' with ' + this.strength + ' power');
+  defender.health -= this.strength;
+  console.log(defender.hero_name + ' health: ', defender.health);
+};
+
+//Instantiate the SuperHeroes
+var drax = new SuperHero('drax', 40);
+var gamora = new SuperHero('gamora', 25);
+var groot = new SuperHero('groot', 60);
+var rocket_racoon = new SuperHero('rocket racoon', 10);
+var star_lord = new SuperHero('star-lord', 30);
+
+// Show the names of the super heroes in the console
+console.log('superhero name: ', drax.hero_name);
+console.log('superhero name: ', gamora.hero_name);
+console.log('superhero name: ', groot.hero_name);
+console.log('superhero name: ', rocket_racoon.hero_name);
+console.log('superhero name: ', star_lord.hero_name);
--- js/tests.js
+++ js/tests.js
@@ -3,3 +3,14 @@ QUnit.test("hello test", function(assert) {
 });
 
 // ADD TESTS HERE
+QUnit.test("SuperHero Name Drax", function(assert) {
+  assert.equal(drax.hero_name, 'drax', "Drax Name is Drax");
+});
+
+QUnit.test("SuperHero Name Gamora", function(assert) {
+  assert.equal(gamora.hero_name, 'gamora', "Gamora Name is Gamora");
+});
+
+QUnit.test("SuperHero Name Groot", function(assert) {
+  assert.equal(groot.hero_name, 'groot', "Drax Name is Groot");
+});
