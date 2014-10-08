b29f43ff4d812cd9de39cf85d6a833598038b321
tsjoberg <tomgsjoberg@gmail.com>
2014-10-07 20:05:10 -0400

    OOP v1 & v2
    Avengers!

--- js/super_hero.js
+++ js/super_hero.js
@@ -1,1 +1,27 @@
-// var SuperHero = ...
+var SuperHero = function(name){
+  this.name = name;
+  this.health = 100;
+
+  this.superShout = function(){
+    return "I am Super " + this.name;
+  };
+  
+  var Avengers = ["Thor", "Iron Man", "Captain America", "Hulk", "Black Widow", "Hawkeye"];
+  if (Avengers.indexOf(this.name) > -1){
+    this.isAvenger = true;
+  } else {
+    this.isAvenger = false;
+  }
+};
+
+var batman = new SuperHero("Batman");
+var thor = new SuperHero("Thor");
+
+SuperHero.prototype.attack = function(victim, power){
+  victim.health = victim.health - power;
+  if (victim.health > 0){
+    return false;
+  } else {
+    return true;
+  }
+};
--- js/tests.js
+++ js/tests.js
@@ -3,3 +3,20 @@ QUnit.test("hello test", function(assert) {
 });
 
 // ADD TESTS HERE
+
+QUnit.test("V1 Tests", function(assert) {
+  assert.strictEqual(thor.name, "Thor", "Name Property");
+  assert.strictEqual(batman.superShout(), "I am Super Batman", "Shout name Method");
+  assert.strictEqual(thor.isAvenger, true, "Check if Avenger");
+  assert.strictEqual(batman.isAvenger, false, "Check if Avenger");
+});
+
+QUnit.test("V2 Tests", function(assert) {
+  //setup
+  var hero1 = new SuperHero("hero1");
+  var hero2 = new SuperHero("hero2");
+
+  //tests
+  assert.strictEqual(hero1.attack(hero2, 100), true, "Defeated");
+  assert.strictEqual(hero2.attack(hero1, 90), false, "Alive");
+});
