f126925165a025807b53790d63728ccb65efa778
Price <205605@carolyn-price-1046.nyhq.nytint.com>
2014-10-08 17:07:11 -0400

    Some initial methods and tests

--- js/super_hero.js
+++ js/super_hero.js
@@ -1,1 +1,33 @@
-// var SuperHero = ...
+var SuperHero = function() {
+    this.age =  20;
+    this.superPower = '';
+    this.kryptonite = '';
+    this.peopleSaved = 0;
+
+
+    this.getAge = function() {
+        return this.age;
+    };
+
+    this.setAge = function(age) {
+        this.age = age;
+    }
+
+    this.getSuperPower = function() {
+        return this.superPower;
+    };
+
+   this.setSuperPower = function(power) {
+        this.superPower = power;
+    };
+
+    this.savePerson = function() {
+        this.peopleSaved++;
+    };
+
+    this.getPeopleSaved = function() {
+        return this.peopleSaved;
+    };
+
+
+};
--- js/tests.js
+++ js/tests.js
@@ -1,5 +1,16 @@
 QUnit.test("hello test", function(assert) {
   assert.strictEqual(1 + 1, 2, "One plus one is two");
 });
+QUnit.test('save person test', function(assert){
+    var superman = new SuperHero();
+    superman.savePerson();
+    assert.strictEqual(1, superman.getPeopleSaved(), 'save a person');
+});
+QUnit.test('set age test', function(assert){
+    var spiderman = new SuperHero();
+    spiderman.setAge(30);
+    assert.strictEqual(30, spiderman.getAge(), 'age should update');
+});
+
 
 // ADD TESTS HERE
