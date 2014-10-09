93744784ab108329c852b87f196d2fbb21432c11
Jon Culver <jonathanhculver@gmail.com>
2014-10-08 23:29:54 -0400

    add super hero class and tests

--- js/super_hero.js
+++ js/super_hero.js
@@ -1,1 +1,56 @@
-// var SuperHero = ...
+
+var oop = window.oop || {};
+
+oop.SuperHero = (function(heroName, power, level) {
+	var self = {};
+	var name,
+		power,
+		strengthLevel;
+
+	var init = function(heroName, power, level) {
+		/* defaults */
+		heroName = (heroName === undefined ? 'batman' : heroName);
+		power = (power === undefined ? 'money' : power);
+		level = (level === undefined ? 1 : level); 
+
+		setName(heroName);
+		setPower(power);
+		setStrength(level);
+	};
+
+	self.getName = function() {
+		return name;
+	};
+
+	self.getPower = function() {
+		return power;
+	};
+
+	self.getStrength = function() {
+		return strengthLevel;
+	};
+
+	self.powerUp = function() {
+		return ++strengthLevel;
+	};
+
+	var setName = function(heroName) {
+		name = heroName;
+		return name;
+	};
+
+	var setPower = function(heroPower) {
+		power = heroPower;
+		return power;
+	};
+
+	var setStrength = function(level) {
+		strengthLevel = level;
+		return level;
+	};
+
+	init(heroName, power, level);
+
+	return self;
+
+});
--- js/tests.js
+++ js/tests.js
@@ -1,5 +1,21 @@
-QUnit.test("hello test", function(assert) {
-  assert.strictEqual(1 + 1, 2, "One plus one is two");
+var spiderman = new oop.SuperHero('spiderman', 'spider skills', 3);
+var batman = new oop.SuperHero();
+
+QUnit.test("Test Init", function(assert) {
+  assert.strictEqual(spiderman.getName(), 'spiderman', "Name is spiderman");
+  assert.strictEqual(spiderman.getStrength(), 3, "Strength is 3");
+  assert.strictEqual(spiderman.getPower(), 'spider skills', "Has spider skills");
 });
 
-// ADD TESTS HERE
+QUnit.test("Test PowerUp", function(assert) {
+  assert.strictEqual(spiderman.powerUp(), 4, "Strength increased from 3 to 4");
+  assert.strictEqual(spiderman.powerUp(), 5, "Strength increased from 4 to 5");
+});
+
+QUnit.test("Test Defaults", function(assert) {
+  assert.strictEqual(batman.getName(), 'batman', "Name is batman");
+  assert.strictEqual(batman.getStrength(), 1, "Strength is 3");
+  assert.strictEqual(batman.getPower(), 'money', "Has money");
+});
+
+
