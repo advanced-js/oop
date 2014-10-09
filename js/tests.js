94f3fce0ffafd9b16e63c74907fda1e217ece7a1
Suzie Grange <suzie.g@me.com>
2014-10-08 21:45:57 -0400

    basic tests

--- js/tests.js
+++ js/tests.js
@@ -2,4 +2,12 @@ QUnit.test("hello test", function(assert) {
   assert.strictEqual(1 + 1, 2, "One plus one is two");
 });
 
-// ADD TESTS HERE
+QUnit.test("Do we know who is who?", function(assert) {
+  assert.strictEqual(superMan.name, "superman", "superman has the right name");
+  assert.strictEqual(doctorEvil.name, "doctorevil", "doctor evil has the right name");
+});
+
+QUnit.test("Did good win over evil?", function(assert) {
+  assert.strictEqual(superMan.alive, true, "superman is alive");
+  assert.strictEqual(doctorEvil.alive, false, "drevil is dead");
+});
