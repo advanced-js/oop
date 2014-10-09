5a40a95cce70d0871c525cb39bbc1461cead1404
Jean Kim <jeankim29@gmail.com>
2014-10-09 03:43:37 -0400

    SuperHero OOP hw

--- js/super_hero.js
+++ js/super_hero.js
@@ -1,1 +1,18 @@
-// var SuperHero = ...
+var SuperHero = function(name, city) {
+	this.name = name;
+	this.city = city;
+
+	return this.name + ' protects ' + this.city;
+}
+
+SuperHero.prototype.disguise = function(alias) {
+	this.name = alias;
+}
+
+SuperHero.prototype.fight = function(enemy) {
+	return (this.name + ' punches ' + enemy.name + ' in the face!!');
+}
+
+SuperHero.prototype.motivate = function(friend) {
+	return("Don't be such a pansy, " + friend.name + "! You have to protect " + friend.city + "!!");
+}
--- js/tests.js
+++ js/tests.js
@@ -1,5 +1,38 @@
-QUnit.test("hello test", function(assert) {
-  assert.strictEqual(1 + 1, 2, "One plus one is two");
+test("Superman becomes Clark Kent", function() {
+  // setup
+  var superman = new SuperHero("Superman", "Metropolis");
+  // action
+  superman.disguise("Clark Kent");
+  // assertion
+  equal(superman.name, "Clark Kent");;
 });
 
-// ADD TESTS HERE
+test("Batman becomes Bruce Wayne", function() {
+  // setup
+  var batman = new SuperHero("Batman", "Gotham");
+  // action
+  batman.disguise("Bruce Wayne");
+  // assertion
+  equal(batman.name, "Bruce Wayne");;
+});
+
+test("Superman fights Batman", function() {
+  // setup
+  var superman = new SuperHero("Superman", "Metropolis");
+  var batman = new SuperHero("Batman", "Gotham");
+  // action
+  var fightResult = superman.fight(batman);
+  // assertion
+  equal(fightResult, "Superman punches Batman in the face!!");
+});
+
+test("Superman motivates Batman", function() {
+  // setup
+  var superman = new SuperHero("Superman", "Metropolis");
+  var batman = new SuperHero("Batman", "Gotham");
+  // action
+  var speech = superman.motivate(batman);
+  // assertion
+  equal(speech, "Don't be such a pansy, Batman! You have to protect Gotham!!");
+});
+
