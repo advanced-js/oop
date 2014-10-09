7c43881805b96e5c74c190e3044a5ec8917b8c0d
alexoviedo <alexoviedo999@gmail.com>
2014-10-08 23:31:04 -0400

    initial commit

--- js/super_hero.js
+++ js/super_hero.js
@@ -1,1 +1,34 @@
 // var SuperHero = ...
+
+// object constructor
+var SuperHero = function(name,alias, power){
+	this.name = name;
+	this.alias = alias;
+	this.power = power;
+	this.SuperHeroInfo = SuperHeroInfo;
+}
+
+
+// object method
+function SuperHeroInfo() {
+	line1 = "Name: " + this.name + "</br>";
+	line2 = "Alias: " + this.alias + "</br>";
+	line3 = "Birth Place: " + this.birthPlace + "</br>";
+	document.write(line1, line2, line3);
+	return this.name;
+}
+
+// create object instance
+var superman = new SuperHero("Superman", "Clark", "Super Strength");
+
+// extend object instance
+superman.birthPlace = "Crypton";
+
+// call method on the instance of the object
+superman.SuperHeroInfo();
+
+SuperHero.prototype.fly = function() {
+	console.log('up and away');
+
+}();
+
--- js/tests.js
+++ js/tests.js
@@ -1,5 +1,13 @@
-QUnit.test("hello test", function(assert) {
-  assert.strictEqual(1 + 1, 2, "One plus one is two");
-});
+// QUnit.test("hello test", function(assert) {
+//   assert.strictEqual(1 + 1, 2, "One plus one is two");
+// });
 
 // ADD TESTS HERE
+
+test('superman.SuperHeroInfo()', function() {
+	equal(superman.name, "Superman", "Superhero Name");
+	equal(superman.birthPlace, "Crypton", "Superhero Name");
+	equal(line1, "Name: Superman</br>");
+	equal(line2, "Alias: Clark</br>")
+	equal(line3, "Birth Place: Crypton</br>")
+});
