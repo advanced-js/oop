f7b9e5117d82355eadf6645862ff835c8f1eed80
Emily O'Brien <emily.obrien@refinery29.com>
2014-10-09 16:55:22 -0400

    OOP

--- js/super_hero.js
+++ js/super_hero.js
@@ -1,1 +1,18 @@
-// var SuperHero = ...
+var SuperHero = function SuperHero(name, power, health){
+	this.name = name;
+	this.power = power;
+	this.health = health || 100;
+	this.attack = function(target){
+
+		console.log(target.name + ' is being attacked by ' + this.name);
+		
+		target.health -= 10;
+		
+		console.log(target.name + ' now has health of ' + target.health);
+	}
+};
+
+var batman = new SuperHero('Batman', 'being wealthy');
+var superman = new SuperHero('Superman', 'being an alien');
+
+batman.attack(superman);
