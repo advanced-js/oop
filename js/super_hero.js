37ba294b1ef330c8ca21c824ecfe8207267f7bbe
Rachel Shatkin <shatkin.r@gmail.com>
2014-10-16 17:17:33 -0400

    adding testing

--- js/super_hero.js
+++ js/super_hero.js
@@ -4,17 +4,15 @@ var SuperHero = function SuperHero(name, move, lives, numAttacks) {
   this.move = move;
   this.lives = lives || 3;
   this.numAttacks = numAttacks || 3;
-
-  this.attack = function(target) {
-
-    console.log(target.name + ' is being ' + move + 'ed by ' + this.name + '!');
-    target.lives -= 1;
-    console.log(target.name + ' now has ' + target.lives + ' lives!');
-    target.numAttacks -=1;
-    console.log(this.name + ' now has ' + target.numAttacks + ' attacks!');
-  };
 };
 
+SuperHero.prototype.attack = function(target) {
+  console.log(target.name + ' is being ' + this.move + 'ed by ' + this.name + '!');
+  target.lives -= 1;
+  console.log(target.name + ' now has ' + target.lives + ' lives!');
+  target.numAttacks -=1;
+  console.log(this.name + ' now has ' + target.numAttacks + ' attacks!');
+};
 
 var emmaStone = new SuperHero('Emma Stone', 'sucker punch');
 var michelleWilliams = new SuperHero('Michelle Williams', 'curb stomp');
