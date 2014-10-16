6f47a539c6b72f270f235540502bd84366116c42
Eric <et1187@nyu.edu>
2014-10-16 08:44:29 -0400

    Implmented superhero class

--- js/super_hero.js
+++ js/super_hero.js
@@ -1,1 +1,38 @@
-// var SuperHero = ...
+var SuperHero = function SuperHero()
+{
+  this.dialog = [];
+  this.talkIndex = 0;
+}
+SuperHero.prototype.talks = function()
+{
+  var saids = this.dialog[this.talkIndex];
+  this.talkIndex++;
+  console.log(saids);
+  return saids;
+}
+
+Cartman.prototype = new SuperHero();
+Cartman.prototype.constructor = Cartman;
+function Cartman()
+{
+  this.dialog.push("Kyle...");
+  this.dialog.push("Don't do this Kyle!");
+  this.dialog.push("I am warning you Kyle!");
+}
+
+Kyle.prototype = new SuperHero();
+Kyle.prototype.constructor = Kyle;
+function Kyle()
+{
+  this.dialog.push("What Cartman?!");
+  this.dialog.push("Don't tell me what to do!");
+  this.dialog.push("You can't stop me!");
+}
+
+var cartman = new Cartman();
+var kyle = new Kyle();
+
+for(var i = 0; i < 3; i++){
+  cartman.talks();
+  kyle.talks();
+}
