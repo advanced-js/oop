15f199b4566a7bb77f00067a3a9730f37b024b82
Emily Maskin <emily.maskin@gmail.com>
2015-04-27 21:45:09 -0400

    create SuperHero class and tests

--- js/super_hero.js
+++ js/super_hero.js
@@ -1,1 +1,44 @@
-// var SuperHero = ...
+var SuperHero = function(name, ability, isVillain) {
+	this.name = name;
+	this.ability = ability;
+	this.partnerWith = function(otherHero) {
+		return 'Crime-fighting duo ' + this.name + ' and ' + otherHero.name + '!';
+	}
+	this.attack = function(otherHero) {
+		return this.name + ' has attacked ' + otherHero.name + ' using ' + this.ability;
+	}
+	this.win = function() {
+		return this.name + ' saves the day!';
+	}
+	this.teamUpWith = function(teammates) {
+		var sizeOfTeam = arguments.length;
+		var aboutTeam = this.name + ' has teamed up with ';
+		for (var i=0; i < sizeOfTeam; i++) {
+			aboutTeam += arguments[i].name ;
+			if (i === sizeOfTeam - 2) {
+				aboutTeam += ' and ';
+			} else if (sizeOfTeam > 2 && i < sizeOfTeam - 1) {
+				aboutTeam += ', ';
+			}
+		}
+		return aboutTeam;
+	}
+};
+
+var Thor = new SuperHero('Thor', 'hammer', false);
+var Hulk = new SuperHero('The Incredible Hulk', 'rage', false);
+var Captain = new SuperHero('Captain America', 'patriotism', false)
+var IronMan = new SuperHero('Iron Man', 'tech', false);
+var Loki = new SuperHero('Loki', 'tesseract', true);
+
+Thor.teamUpWith(Hulk);
+Thor.teamUpWith(IronMan, Captain, Hulk);
+Thor.attack(Loki);
+Hulk.attack(Loki);
+Captain.attack(Loki);
+IronMan.attack(Loki);
+Thor.win();
+Hulk.win();
+Captain.win();
+IronMan.win();
+
--- js/tests.js
+++ js/tests.js
@@ -1,5 +1,14 @@
 QUnit.test("hello test", function(assert) {
-  assert.strictEqual(1 + 1, 2, "One plus one is two");
+	var Hulk = new SuperHero('The Incredible Hulk', 'rage', false);
+	var Thor = new SuperHero('Thor', 'hammer', false);
+	var Captain = new SuperHero('Captain America', 'patriotism', false);
+	var IronMan = new SuperHero('Iron Man', 'tech', false);
+	var Loki = new SuperHero('Loki', 'tesseract', true);
+
+	assert.strictEqual(Captain.win(), 'Captain America saves the day!');
+	assert.strictEqual(Thor.partnerWith(IronMan), 'Crime-fighting duo Thor and Iron Man!');
+	assert.strictEqual(Hulk.attack(Loki), 'The Incredible Hulk has attacked Loki using rage');
+	assert.strictEqual(Thor.teamUpWith(IronMan, Captain, Hulk), 'Thor has teamed up with Iron Man, Captain America and The Incredible Hulk');
 });
 
 // ADD TESTS HERE
