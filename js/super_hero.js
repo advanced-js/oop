7cd8e78192d4279b9fb5cd8042e4be09206db702
Kobe Osei-Akoto <kobeosei-akoto@Kobes-MacBook-Pro.local>
2015-04-30 16:59:58 -0400

    Adds 1st round of tests

--- js/super_hero.js
+++ js/super_hero.js
@@ -1,1 +1,17 @@
 // var SuperHero = ...
+
+var SuperHero = function(name, age) {
+	this.name = name;
+	this.age = age;
+	return "Superhero created!";
+}
+
+SuperHero.prototype.fly = function() {
+	return "Yes, your superhero can fly!";
+}
+
+SuperHero.prototype.destroy = function() {
+	return "Yes, your superhero can destroy the enemy!";
+}
+
+var superman = new SuperHero("Clark Kent", 27);
--- js/tests.js
+++ js/tests.js
@@ -3,3 +3,22 @@ QUnit.test("hello test", function(assert) {
 });
 
 // ADD TESTS HERE
+
+// Check to see if superman.name is equal
+
+
+QUnit.test('superman.name', function(assert) {
+	assert.strictEqual(superman.name, "Clark Kent", "Superman's name is Clark Kent");
+});
+
+QUnit.test('superman.age', function(assert) {
+	assert.strictEqual(superman.age, 27, "Superman's age is 27");
+});
+
+QUnit.test('superman.fly()', function() {
+	equal(superman.fly(), "Yes, your superhero can fly!", "superhero.fly() outputs correct string!");
+});
+
+QUnit.test('superman.destroy()', function() {
+	equal(superman.destroy(), "Yes, your superhero can destroy the enemy!", "superhero.destroy() outputs correct string!");
+});
