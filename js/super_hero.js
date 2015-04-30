949bbf3f87cf9228b85cbc9bfe6cb3e6b808c113
Hindi Kornbluth <HindiKornbluth@renttherunway.com>
2015-04-29 21:25:03 -0400

    Superhero OOP excercise + tests

--- js/super_hero.js
+++ js/super_hero.js
@@ -1,1 +1,28 @@
 // var SuperHero = ...
+
+var Superhero = function( name, strength, archNemisis, loveInterest, isLoveRequited, catchphrase) {
+  this.name = name;
+  this.archNemisis = archNemisis;
+  this.strength = strength;
+  this.loveInterest = loveInterest;
+  this.isLoveRequited = isLoveRequited;
+  this.catchphrase = catchphrase;
+
+  this.fightSceneAgainst = function(fightAgainst) {
+    var winFightStr = this.name + " punches " + fightAgainst + " and shouts, \"" + this.catchphrase + "\"";
+    var loseFightStr = "Oh no, " + this.name + "\'s strength is too low. " + fightAgainst + " punches them and runs off with " + this.loveInterest + ".";
+    if (this.strength > fightAgainst.strength) {
+      fightAgainst.strength--;
+      return winFightStr;
+    } else {
+      this.strength--;
+      return loseFightStr;
+    }
+  }
+
+  this.LoveScene = function() {
+    var loveRequitedStr = this.name + " approaches " + this.loveInterest + " and (after obtaining consent) kisses them, and whispers \"" + this.catchphrase + ".\" \"Why do you do that? It's not sexy.\", " + this.loveInterest + " responds.";
+    var loveUnrequitedStr = this.name + "looks lovingly at " + this.loveInterest + ", dreaming of the day they can be together."
+    return this.isLoveRequited ? loveRequitedStr : loveUnrequitedStr;
+  }
+};
--- js/tests.js
+++ js/tests.js
@@ -1,5 +1,24 @@
-QUnit.test("hello test", function(assert) {
-  assert.strictEqual(1 + 1, 2, "One plus one is two");
+QUnit.test("superHero assignment test", function(assert) {
+  var BuffySummers = new Superhero("Buffy", 10, "The Master", "Angel", true, "I\'m Buffy, the vampire slayer");
+  assert.strictEqual(BuffySummers.loveInterest, "Angel", "Superhero object gets created and assigns string attribute correctly");
+  assert.strictEqual(BuffySummers.isLoveRequited, true, "assigns boolean attribute correctly");
+  //I don't think I'd have to test all of these; once I know that two data types have been successfully assigned I'm happy
 });
 
-// ADD TESTS HERE
+QUnit.test("fightScene test", function(assert) {
+  var DrHorrible = new Superhero("Dr. Horrible", 10, "Captain Hammer", "Penny", false, "I\'ve got a PhD in Horribleness.");
+  var CaptainHammer = new Superhero("Captain Hammer", 11, "Dr. Horrible", "Penny", true, "*These* are not the hammer.");
+  assert.strictEqual(DrHorrible.strength, 10, "Strength gets assinged correctly.");
+  DrHorrible.fightSceneAgainst(CaptainHammer);
+  assert.strictEqual(DrHorrible.strength, 9, "strength loss after losing a fight");
+  assert.strictEqual(CaptainHammer.strength, 11, "No strength loss after winning a fight");
+
+});
+
+
+
+QUnit.test("loveScene test", function(assert) {
+  var BlackWidow = new Superhero("Black Widow", 1000, "The Patriarchy", "Hawkeye", true, "something russian idk");
+  var LoveSceneStr = BlackWidow.LoveScene();
+  assert.strictEqual(LoveSceneStr, "Black Widow approaches Hawkeye and (after obtaining consent) kisses them, and whispers \"something russian idk.\" \"Why do you do that? It's not sexy.\", Hawkeye responds." , "returns love scene string correctly");
+});
