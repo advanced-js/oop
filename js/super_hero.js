34f25c7af699977ee4d5ea54c39f665f96e16b90
Kobe Osei-Akoto <kobeosei-akoto@Kobes-MacBook-Pro.local>
2015-04-30 17:49:27 -0400

    Getting tests to pass?

--- js/super_hero.js
+++ js/super_hero.js
@@ -4,19 +4,19 @@ var SuperHero = function(name, age) {
 	this.name = name;
 	this.age = age;
 	return "Superhero created!";
-}
+};
 
 SuperHero.prototype.fly = function() {
 	return "Yes, your superhero can fly!";
-}
+};
 
 SuperHero.prototype.destroy = function() {
 	return "Yes, your superhero can destroy the enemy!";
-}
+};
 
 SuperHero.prototype.attack = function() {
 	return "Attack the other superhero!";
-}
+};
 
 var superman = new SuperHero("Clark Kent", 27);
 var batman = new SuperHero("John Wayne", 34);
