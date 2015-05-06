a2034e0e220edd560d33448f4a55726fdfd95a2e
Alek <agrinb@gmail.com>
2015-05-06 17:15:32 -0400

    V2 complete

--- js/super_hero.js
+++ js/super_hero.js
@@ -1,1 +1,21 @@
-// var SuperHero = ...
+var SuperHero = function(name, energy, canFly) {
+  this.name = name;
+  this.energy = energy;
+  this.canFly = canFly;
+  this.giveReport = function () {
+    console.log('I\'m ' + name + ', I currently have ' + energy + ' percent of energy!')
+    return 'I\'m ' + name + ', I currently have ' + energy + ' percent of energy!'
+  };
+  this.attack = function(superhero) {
+    this.energy -= 2
+    superhero.energy -= 5    
+  };
+
+  this.drinkElixir = function(){
+    while (this.energy < 100) {
+      this.energy++;
+    }
+  };
+  return this
+};
+
--- js/tests.js
+++ js/tests.js
@@ -2,4 +2,40 @@ QUnit.test("hello test", function(assert) {
   assert.strictEqual(1 + 1, 2, "One plus one is two");
 });
 
+QUnit.test("Superhero has a name", function(assert) {
+  var superMan = new SuperHero("Super Man", 100, true);
+  assert.strictEqual(superMan.name, "Super Man", "Returns name");
+});
+
+QUnit.test("Superhero has a name", function(assert) {
+  var superMan = new SuperHero("Super Man", 100, true);
+  assert.strictEqual(superMan.energy, 100, "Returns Energy" );
+});
+
+QUnit.test("Superhero has a name", function(assert) {
+  var superMan = new SuperHero("Super Man", 100, true);
+  assert.strictEqual(superMan.giveReport(), "I'm Super Man, I currently have 100 percent of energy!", "Returns Report Message" );
+});
+
+QUnit.test("Superhero has a name", function(assert) {
+  var batMan = new SuperHero("Bat Man", 80, false);
+  assert.strictEqual(batMan.giveReport(), "I'm Bat Man, I currently have 80 percent of energy!", "Returns Report Message" );
+});
+
+QUnit.test("Superhero has a name", function(assert) {
+  var superMan = new SuperHero("Super Man", 100, true);
+  var batMan = new SuperHero("Bat Man", 80, false);
+  batMan.attack(superMan);
+  assert.strictEqual(superMan.energy, 95, "Atack function decreases energy" );
+  assert.strictEqual(batMan.energy, 78, "Atack function decreases energy on self" );
+});
+
+QUnit.test("Superhero has a name", function(assert) {
+  var superMan = new SuperHero("Super Man", 100, true);
+  var batMan = new SuperHero("Bat Man", 80, false);
+  batMan.drinkElixir();
+  assert.strictEqual(batMan.energy, 100, "drinkElixir method returns energy to 100%" );
+});
+
+
 // ADD TESTS HERE
