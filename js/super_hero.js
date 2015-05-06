dcc8201f14eb18756eca4a8f20be8c859aab11db
Hindi Kornbluth <HindiKornbluth@renttherunway.com>
2015-05-05 21:09:32 -0400

    V3 of OOP excercise

--- js/super_hero.js
+++ js/super_hero.js
@@ -1,8 +1,7 @@
 // var SuperHero = ...
 
-var Superhero = function( name, strength, archNemisis, loveInterest, isLoveRequited, catchphrase) {
+var Superhero = function( name, strength, loveInterest, isLoveRequited, catchphrase) {
   this.name = name;
-  this.archNemisis = archNemisis;
   this.strength = strength;
   this.loveInterest = loveInterest;
   this.isLoveRequited = isLoveRequited;
@@ -21,8 +20,24 @@ var Superhero = function( name, strength, archNemisis, loveInterest, isLoveRequi
   };
 
   this.LoveScene = function() {
-    var loveRequitedStr = this.name + " approaches " + this.loveInterest + " and (after obtaining consent) kisses them, and whispers \"" + this.catchphrase + ".\" \"Why do you do that? It's not sexy.\", " + this.loveInterest + " responds.";
+    var loveRequitedStr = this.name + " approaches " + this.loveInterest + " and (after obtaining consent) kisses them, and whispers \"" + this.catchphrase + ".\"";
     var loveUnrequitedStr = this.name + "looks lovingly at " + this.loveInterest + ", dreaming of the day they can be together.";
     return this.isLoveRequited ? loveRequitedStr : loveUnrequitedStr;
   };
+};
+
+var Batman = function(){};
+
+Batman.prototype = Object.create(Superhero.prototype);
+
+Batman.prototype.numParents = function() {
+  return 0;
+};
+
+Batman.prototype.Director = function(year) {
+  if (year > 2005) {
+    return "Christopher Nolan";
+  } if ( 1989 < year < 1995) {
+    return "Tim Burton";
+  }
 };
--- js/tests.js
+++ js/tests.js
@@ -1,24 +1,36 @@
-QUnit.test("superHero assignment test", function(assert) {
-  var BuffySummers = new Superhero("Buffy", 10, "The Master", "Angel", true, "I\'m Buffy, the vampire slayer");
-  assert.strictEqual(BuffySummers.loveInterest, "Angel", "Superhero object gets created and assigns string attribute correctly");
+QUnit.test("superHero object", function(assert) {
+  var BuffySummers = new Superhero("Buffy", 50, "Angel", true, "I\'m Buffy, the vampire slayer");
+  assert.strictEqual(BuffySummers.loveInterest, "Angel", "gets created and assigns string attribute correctly");
   assert.strictEqual(BuffySummers.isLoveRequited, true, "assigns boolean attribute correctly");
   //I don't think I'd have to test all of these; once I know that two data types have been successfully assigned I'm happy
 });
 
-QUnit.test("fightScene test", function(assert) {
-  var DrHorrible = new Superhero("Dr. Horrible", 10, "Captain Hammer", "Penny", false, "I\'ve got a PhD in Horribleness.");
-  var CaptainHammer = new Superhero("Captain Hammer", 11, "Dr. Horrible", "Penny", true, "*These* are not the hammer.");
+QUnit.test("fightScene function", function(assert) {
+  var DrHorrible = new Superhero("Dr. Horrible", 10, "Penny", false, "I\'ve got a PhD in Horribleness.");
+  var CaptainHammer = new Superhero("Captain Hammer", 11, "Penny", true, "*These* are not the hammer.");
   assert.strictEqual(DrHorrible.strength, 10, "Strength gets assinged correctly.");
   DrHorrible.fightSceneAgainst(CaptainHammer);
-  assert.strictEqual(DrHorrible.strength, 9, "strength loss after losing a fight");
-  assert.strictEqual(CaptainHammer.strength, 11, "No strength loss after winning a fight");
+  assert.strictEqual(DrHorrible.strength, 9, "results in strength loss after losing a fight");
+  assert.strictEqual(CaptainHammer.strength, 11, "results in no strength loss after winning a fight");
 
 });
 
 
 
-QUnit.test("loveScene test", function(assert) {
-  var BlackWidow = new Superhero("Black Widow", 1000, "The Patriarchy", "Hawkeye", true, "something russian idk");
+QUnit.test("loveScene function", function(assert) {
+  var BlackWidow = new Superhero("Black Widow", 1000, "Hawkeye", true, "something russian idk");
   var LoveSceneStr = BlackWidow.LoveScene();
-  assert.strictEqual(LoveSceneStr, "Black Widow approaches Hawkeye and (after obtaining consent) kisses them, and whispers \"something russian idk.\" \"Why do you do that? It's not sexy.\", Hawkeye responds." , "returns love scene string correctly");
+  assert.strictEqual(LoveSceneStr, "Black Widow approaches Hawkeye and (after obtaining consent) kisses them, and whispers \"something russian idk.\"" , "returns love scene string correctly");
+});
+
+QUnit.test("subclass Batman numParents", function(assert) {
+  assert.strictEqual(Batman.prototype.numParents(), 0 , "Batman doesn't have any parents (they were killed)");
+});
+
+QUnit.test("subclass Batman Director function", function(assert) {
+  var recentMovies = Batman.prototype.Director(2013);
+  var earlyNinetiesMovies = Batman.prototype.Director(1990);
+  assert.strictEqual(recentMovies, "Christopher Nolan" , "The Dark Knight movies were directed by");
+  assert.strictEqual(earlyNinetiesMovies, "Tim Burton" , "Batman Begins and such");
+
 });
