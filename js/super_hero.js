4d1ed6e12b2c2dcb97376eeeedcc82211328048e
Kobe Osei-Akoto <kobeosei-akoto@Kobes-MacBook-Pro.local>
2015-05-07 17:45:22 -0400

    Adding tests

--- js/super_hero.js
+++ js/super_hero.js
@@ -3,10 +3,12 @@
 var SuperHero = function(name, age) {
 	this.name = name;
 	this.age = age;
+	this.energy = 100;
 	return "Superhero created!";
 };
 
 SuperHero.prototype.fly = function() {
+	this.energy -= 1;
 	return "Yes, your superhero can fly!";
 };
 
@@ -18,5 +20,21 @@ SuperHero.prototype.attack = function() {
 	return "Attack the other superhero!";
 };
 
+var SubHero = function(name, age) {
+	SuperHero.call(this, name, age);
+	this.punkNess = 10;	
+};
+
+SubHero.prototype = Object.create(SuperHero.prototype);
+
+SubHero.prototype.tookSteroids = function() {
+	this.punkNess -=1;
+}
+
+SubHero.prototype.claimingSelf = function() {
+	return "I'm a punk";
+}
+
 var superman = new SuperHero("Clark Kent", 27);
 var batman = new SuperHero("John Wayne", 34);
+var wannabeOne = new SubHero("Gunther", 21);
--- js/tests.js
+++ js/tests.js
@@ -15,8 +15,21 @@ QUnit.test('superman.age', function(assert) {
 	assert.strictEqual(superman.age, 27, "Superman's age is 27");
 });
 
-QUnit.test('superman.fly()', function() {
+QUnit.test('superman.fly() should return a string', function() {
 	equal(superman.fly(), "Yes, your superhero can fly!", "superhero.fly() outputs correct string!");
+});
+
+QUnit.test('superman.fly() should decrease his energy', function() {
+	// superman.fly();
+	// equal(superman.energy, 99, "Energy was reduced");
+	// equal(superman.energy, superman.energy - 1, "Energy was reduced");
+
+	var previousEnergy = superman.energy;
+	superman.fly();
+	expect(superman.energy, previousEnergy - 1)
+
+
+	equal(superman.energy, 98, "Energy was reduced by one again");
 });
 
 QUnit.test('superman.destroy()', function() {
@@ -25,4 +38,29 @@ QUnit.test('superman.destroy()', function() {
 
 QUnit.test('SuperHero.prototype.attack()', function(assert) {
 	assert.strictEqual(superman.attack, batman.attack, "Superman and Batman are attacking each other!");
+});
+
+QUnit.test("wannabeOne.name", function(assert) {
+	assert.strictEqual(wannabeOne.name, "Gunther", "wannabeOne's name is Gunther");
+});
+
+QUnit.test("wannabeOne.age", function(assert) {
+	assert.strictEqual(wannabeOne.age, 21, "wannabeOne's age is 21");
+});
+
+QUnit.test("wannabeOne.attack() should return a string", function() {
+	equal(wannabeOne.attack(), "Attack the other superhero!", "wannabeOne.attack() outputs correct string!");
+});
+
+QUnit.test("wannabeOne.tookSteroids() should decrease his punkNess", function() {
+
+	var previousPunkness = wannabeOne.punkNess;
+	wannabeOne.tookSteroids();
+	expect(wannabeOne.punkNess, previousPunkness - 1);
+
+	equal(wannabeOne.punkNess, 9, 'wannabeOne\'s punkNess reduced by One');
+});
+
+QUnit.test("wannabeOne.claimingSelf() should return a string", function() {
+	equal(wannabeOne.claimingSelf(), "I'm a punk", 'wannabeOne should return a string that says I\'m a punk');
 });
