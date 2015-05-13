c48ef8a57838d3636128e97a3597c34332b1eb63
Alek <agrinb@gmail.com>
2015-05-12 20:07:22 -0400

    Fixed all errors

--- js/super_hero.js
+++ js/super_hero.js
@@ -6,8 +6,8 @@ var SuperHero = function(name, energy, canFly) {
 
 
 SuperHero.prototype.attack = function(superhero) {
-    this.energy -= 2
-    superhero.energy -= 5    
+    this.energy -= 2;
+    superhero.energy -= 5;    
   };
 
 SuperHero.prototype.drinkElixir = function(){
@@ -17,17 +17,17 @@ SuperHero.prototype.drinkElixir = function(){
   };
 
 SuperHero.prototype.giveReport = function () {
-    console.log('I\'m ' + this.name + ', I currently have ' + this.energy + ' percent of energy!')
-    return 'I\'m ' + this.name + ', I currently have ' + this.energy + ' percent of energy!'
+    console.log('I\'m ' + this.name + ', I currently have ' + this.energy + ' percent of energy!');
+    return 'I\'m ' + this.name + ', I currently have ' + this.energy + ' percent of energy!';
   };
 
-function Villain() {
+function Villain(name) {
   SuperHero.call(this, name);
   this.intentions = "evil";
   this.kill = function(superhero){
     superhero.energy = 0;
   };
-};
+}
 
 Villain.prototype = Object.create(SuperHero.prototype);
 
