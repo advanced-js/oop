aa37a34d2a752e1a38252ea9caf0699e7901e7a3
yuyudesigns <yuyeonkim@gmail.com>
2015-05-17 16:54:16 -0400

    Update super_hero.js

--- js/super_hero.js
+++ js/super_hero.js
@@ -1,1 +1,27 @@
 // var SuperHero = ...
+var superHero = function (name, ability) {
+  this.name = name;
+  this.ability = ability;
+}
+
+SuperHero.prototype.fly = function() {
+  return this.name + " can fly everywhere! ";
+}
+
+SuperHero.prototype.fast = function() {
+  return this.name + " can move fast! ";
+}
+
+SuperHero.prototype.crush = function() {
+  return this.name + " can crush anything! ";
+}
+
+SuperHero.prototype.superPower = function() {
+  return this.name + " can read people's mind with his ability " + this.ability;
+}
+
+SuperHero.prototype.notHuman = function() {
+  return this.name + " is not a human! ";
+} 
+
+var superMan = new superHero("Superman", "superpower");
