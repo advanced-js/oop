17dfadd2d881bda24497a9a61e9b72585ed41fd4
yuyudesigns <yuyeonkim@gmail.com>
2015-05-17 17:21:16 -0400

    Update tests.js

--- js/tests.js
+++ js/tests.js
@@ -3,3 +3,14 @@ QUnit.test("hello test", function(assert) {
 });
 
 // ADD TESTS HERE
+QUnit.test("superhero's name", function(assert) {
+  assert.strictEqual(superMan.name, "Superman", "SuperHero is Superman");
+});
+
+QUnit.test("superhero's ability", function(assert) {
+  assert.strictEqual(superMan.ability, "superpower", "SuperHero's ability is superpower");
+});
+
+QUnit.test("superhero reads mind", function(assert) {
+  assert.strictEqual(superMan.superPower(), "Superman can read minds with his ability superpower", "SuperHero can read minds with his ability superpower");
+});
