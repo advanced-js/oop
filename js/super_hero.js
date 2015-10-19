877b8afb936694b9368f7607c6fae4c8bfd4f337
mnsinger <mnsinger@gmail.com>
2015-10-18 20:53:27 -0400

    OOP js
    Created properties, methods and a method that interacts with another
    SuperHero.

--- js/super_hero.js
+++ js/super_hero.js
@@ -3,6 +3,7 @@ var SuperHero = function (superName, secretIdentity, superPower) {
   this.superName = superName;
   this.secretIdentity = secretIdentity;
   this.superPower = superPower;
+  this.partner = "";
 };
 
 SuperHero.prototype.useSuperPower = function() {
@@ -10,8 +11,17 @@ SuperHero.prototype.useSuperPower = function() {
   return this.superName + " uses super power " + this.superPower;
 };
 
+SuperHero.prototype.teamUp = function(partner) {
+  console.log(this.superName + " is teaming up with " + partner.superName);
+  this.partner = partner.superName;
+  partner.partner = this.superName;
+  return this.superName + " is teaming up with " + partner.superName;
+};
+
 var spiderman = new SuperHero("Spider-Man", "Peter Parker", "shoot spider web");
 var superman = new SuperHero("Superman", "Clark Kent", "super strength");
 
 spiderman.useSuperPower();
 superman.useSuperPower();
+
+superman.teamUp(spiderman);
