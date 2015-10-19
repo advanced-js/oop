7daf7d131021123415b3bd8ab951c96551495754
mnsinger <mnsinger@gmail.com>
2015-10-18 20:33:27 -0400

    Created Superheroes and tests

--- js/super_hero.js
+++ js/super_hero.js
@@ -1,1 +1,17 @@
 // var SuperHero = ...
+var SuperHero = function (superName, secretIdentity, superPower) {
+  this.superName = superName;
+  this.secretIdentity = secretIdentity;
+  this.superPower = superPower;
+};
+
+SuperHero.prototype.useSuperPower = function() {
+  console.log(this.superName + " uses super power " + this.superPower);
+  return this.superName + " uses super power " + this.superPower;
+};
+
+var spiderman = new SuperHero("Spider-Man", "Peter Parker", "shoot spider web");
+var superman = new SuperHero("Superman", "Clark Kent", "super strength");
+
+spiderman.useSuperPower();
+superman.useSuperPower();
--- js/tests.js
+++ js/tests.js
@@ -1,5 +1,13 @@
-QUnit.test("hello test", function(assert) {
+/*QUnit.test("hello test", function(assert) {
   assert.strictEqual(1 + 1, 2, "One plus one is two");
-});
+});*/
+
+
 
 // ADD TESTS HERE
+QUnit.test("create superheroes", function(assert) {
+	var spiderman = new SuperHero("Spider-Man", "Peter Parker", "shoot spider web");
+	assert.strictEqual(spiderman.useSuperPower(), "Spider-Man uses super power shoot spider web", "Spider-Man was created correctly");
+	var superman = new SuperHero("Superman", "Clark Kent", "super strength");
+	assert.strictEqual(superman.useSuperPower(), "Superman uses super power super strength", "Superman was created correctly");
+});
