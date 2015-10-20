37f39fccc834a9afebd9ea282a56c0055621cfec
katelynistoll <Ryan@Ryan-Stolls-MacBook-Pro.local>
2015-10-19 21:48:39 -0400

    Complete: OOP V2
    Still working on getting qUnit test to pass.

--- js/super_hero.js
+++ js/super_hero.js
@@ -1,1 +1,34 @@
-// var SuperHero = ...
+var SuperHero = new Object();
+
+var SuperHero = function(realname, character, age, power) {
+    this.realname = realname;
+    this.character = character;
+    this.age = age + " yrs old";
+    this.power = power;
+};
+
+SuperHero.prototype.talk = function() {
+    console.log("I'm " + this.realname + ", I'm "+ this.age +", and my alter ego is " + this.character + "!");
+};
+
+SuperHero.prototype.newPower = function(power) {
+	this.power = power;
+    console.log("My new power is " + this.power + "!");
+};
+
+SuperHero.prototype.brag = function(friend) {
+  console.log(this.realname + " is bragging to " + friend.realname + " that he is the better super hero!");
+};
+
+
+var hulk = new SuperHero("Bruce Banner", "The Hulk", 34, "Superhuman Strength");
+var flash = new SuperHero("Barry Allen", "The Flash", 26, "Superhuman Speed");
+
+
+hulk.talk(flash);
+hulk.newPower("Resistance to Injury");
+hulk.brag(flash);
+
+flash.talk();
+flash.newPower("Invisibility");
+flash.brag(hulk);
--- js/tests.js
+++ js/tests.js
@@ -1,5 +1,17 @@
-QUnit.test("hello test", function(assert) {
-  assert.strictEqual(1 + 1, 2, "One plus one is two");
-});
+
 
 // ADD TESTS HERE
+
+QUnit.test("hulk and flash are born", function(assert) {
+	var hulk = new SuperHero("Bruce Banner", "The Hulk", 34, "Superhuman Strength");
+	assert.strictEqual(hulk.talk(),"I'm Bruce Banner, I'm 34 yrs old, and my alter ego is The Hulk!", "Hulk is talking correctly.");
+	assert.strictEqual(hulk.newPower("Resistance to Injury"), "My new power is Resistance to Injury!", "Hulk's power was changed correctly.");
+	var flash = new SuperHero("Barry Allen", "The Flash", 26, "Superhuman Speed");
+	assert.strictEqual(flash.talk(), "I'm Barry Allen, I'm 26 yrs old, and my alter ego is The Flash!", "Flash is talking correctly.");
+	assert.strictEqual(flash.newPower("Invisibility"), "My new power is Invisibility!", "Flash's power was changed correctly.");
+});
+
+
+QUnit.test("bragging test", function(assert) {
+	assert.strictEqual(flash.brag(hulk), "Barry Allen is bragging to Bruce Banner that he is the better super hero!", "Flash is definitely bragging to the Hulk!");
+});
