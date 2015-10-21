31a9296086fa03d9ff7677873c96c7072f7e96cf
Alex Chaparro <alex@kargo.com>
2015-10-21 00:39:08 -0400

    working on creating an object

--- js/super_hero.js
+++ js/super_hero.js
@@ -1,1 +1,10 @@
 // var SuperHero = ...
+function begin() {
+	var Superhero= function (firstName) {
+  	this.firstName = firstName;
+  	this.power = teleklensis;
+  	this.sex = male;
+  	console.log('Superhero instantiated');
+	};
+}
+console.log(begin());
