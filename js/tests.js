a7c9150106e9662be4875518c36f90c95dbde7ec
git-ebetts <ebettsmail@gmail.com>
2015-10-22 15:13:42 -0400

    Solution Part 1 and Part 1 test

--- js/tests.js
+++ js/tests.js
@@ -2,4 +2,19 @@ QUnit.test("hello test", function(assert) {
   assert.strictEqual(1 + 1, 2, "One plus one is two");
 });
 
-// ADD TESTS HERE
+// TESTS FOR SOLUTION PART 1
+
+test('assertions', function (superName){
+ok(this.superName = superName,'has property superName');
+ok(true, 'has property secretPower');
+ok(true, 'property isAlive is true');
+ok(true, 'property hasWeapon is true');
+ok(true, 'property hasShield is true');
+})
+
+test('conditional assertions', function (assert){
+	assert.notEqual(false, true, "My weapons will save me");
+	assert.equal(false, false, "Uh oh. This is going to be sticky");
+	assert.equal(true, true, "You'll never Harm Me!!");
+	assert.notEqual(true, false, "You'll never Harm Me!!");
+});
