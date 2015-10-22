0ccf44358954622791f39577bd90c1c7cba78471
sophiavanvalkenburg <sophia.vanvalkenburg@nytimes.com>
2015-10-22 17:56:24 -0400

    create initial SuperHero class w/setters and test script

--- js/super_hero.js
+++ js/super_hero.js
@@ -1,1 +1,24 @@
-// var SuperHero = ...
+
+var SuperHero = function(){
+    this.power;
+    this.name;
+    this.enemy;
+};
+
+SuperHero.prototype = {
+    setPower: function(power){
+        this.power = power;
+    },
+    setName: function(name){
+        this.name = name;
+    },
+    setEnemy: function(enemy){
+        this.enemy = enemy;
+    }
+};
+
+var sh = new SuperHero();
+sh.setPower("foo");
+sh.setName("bar");
+sh.setEnemy("goo");
+console.log(sh);
