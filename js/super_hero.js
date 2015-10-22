5ae23453349b9afe9069779a5158cc7bc01f693c
mariannedo <mariannepdo@gmail.com>
2015-10-22 14:23:22 -0400

    superhero v1?

--- js/super_hero.js
+++ js/super_hero.js
@@ -1,1 +1,26 @@
-// var SuperHero = ...
+var SuperHero = function (name) {
+    this.name = name; 
+    this.superpower = 'Flying'; 
+    this.hurt = false; 
+};
+
+
+// class methods
+SuperHero.prototype.attack = function (enemy) {
+    console.log('Kick! Punch! Bam! Zing!'); 
+    enemy.hurt = true; 
+};
+
+SuperHero.prototype.giveMotivationalSpeech = function () {
+    console.log('The earth is safe again!'); 
+};
+
+
+// instantiate
+var batman = new SuperHero('batman'); 
+var superman = new SuperHero('superman'); 
+
+superman.attack(batman); 
+
+console.log(superman); 
+console.log(batman); // batman is hurt!
