4b98b4f43798efaa8cef6ff6137b19460722a96e
jonCravotta <jcravotta@renttherunway.com>
2015-10-22 18:20:26 -0400

    basic oop example

--- js/super_hero.js
+++ js/super_hero.js
@@ -1,1 +1,12 @@
-// var SuperHero = ...
+ var SuperHero = function(name, strength){
+   this.name = name;
+   this.strength = strength;
+};
+
+SuperHero.prototype.sayHello = function() {
+  console.log("Hello Im " + this.name + " and my strength is " + this.strength);
+};
+
+var superhero = new SuperHero("Superman", "Flying");
+
+superhero.sayHello();
