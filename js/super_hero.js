4490774a5f8746458f41cccd0feef56ccd28604d
sophiavanvalkenburg <sophia.vanvalkenburg@nytimes.com>
2015-10-22 20:18:10 -0400

    add attack and getHit functions + more test methods. remove setEnemy method

--- js/super_hero.js
+++ js/super_hero.js
@@ -1,5 +1,9 @@
 
-var SuperHero = function(){};
+var SuperHero = function(name, power){
+    this.name = name;
+    this.power = power;
+    this.hp = 100;
+};
 
 SuperHero.prototype = {
     setPower: function(power){
@@ -8,13 +12,20 @@ SuperHero.prototype = {
     setName: function(name){
         this.name = name;
     },
-    setEnemy: function(enemy){
-        this.enemy = enemy;
+    getHit: function(amt){
+        this.hp -= amt;
+        console.log(this.name+" hit!");
+        console.log(this.name+" HP: "+this.hp);
+    },
+    attack: function(other, amt){
+        console.log(this.name+" uses "+this.power+" against "+other.name+"!");
+        other.getHit(amt);
     }
 };
 
-var sh = new SuperHero();
-sh.setPower("foo");
-sh.setName("bar");
-sh.setEnemy("goo");
-console.log(sh);
+var vegeta = new SuperHero(name="Vegeta", power="Galick Gun");
+var goku = new SuperHero();
+goku.setName("Goku");
+goku.setPower("Kamehameha");
+goku.attack(vegeta, 25);
+vegeta.attack(goku, 50);
