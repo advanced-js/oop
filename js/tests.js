2207ae44989be70b2b913c9aa0d67c4d6dd0f60b
Alex Chaparro <alex@kargo.com>
2015-10-29 13:43:28 -0400

    got the methods to pass but now I need to the attack method

--- js/tests.js
+++ js/tests.js
@@ -1,5 +1,18 @@
-QUnit.test("hello test", function(assert) {
+QUnit.test("SuperHero Object", function(assert) {
   assert.strictEqual(1 + 1, 2, "One plus one is two");
 });
 
 // ADD TESTS HERE
+
+QUnit.test("superHero object", function(assert) {
+	var x = new SuperHero("Superman", "God");
+ 	assert.strictEqual(x.title, "Superman");
+ 	assert.strictEqual(x.power, "God");
+ 	assert.strictEqual(x.attack(), this.health);
+ });
+
+// QUnit.test("loveScene function", function(assert) {
+//   var BlackWidow = new Superhero("Black Widow", 1000, "Hawkeye", true, "something russian idk");
+//   var LoveSceneStr = BlackWidow.LoveScene();
+//   assert.strictEqual(LoveSceneStr, "Black Widow approaches Hawkeye and (after obtaining consent) kisses them, and whispers \"something russian idk.\"" , "returns love scene string correctly");
+// });
