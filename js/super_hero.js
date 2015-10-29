5bfde1b8681e2c0fcb2cbc603466b5fc0f5c56ed
jonCravotta <jcravotta@renttherunway.com>
2015-10-29 16:08:41 -0400

    oop with attacking function

--- js/super_hero.js
+++ js/super_hero.js
@@ -1,12 +1,24 @@
- var SuperHero = function(name, strength){
+ var SuperHero = function(name, attackPoints, health){
    this.name = name;
-   this.strength = strength;
+   this.attackPoints = attackPoints;
+   this.health = health;
 };
 
 SuperHero.prototype.sayHello = function() {
-  console.log("Hello Im " + this.name + " and my strength is " + this.strength);
+  console.log("Hello Im " + this.name + " and my attack point is  " + this.attackPoints);
 };
 
-var superhero = new SuperHero("Superman", "Flying");
+SuperHero.prototype.attack = function(victim) {
+  victim.health = victim.health - this.attackPoints;
+  console.log(victim.name + " got attacked by " + this.name + ", " + victim.name + "'s health is now "  + victim.health);
+};
 
-superhero.sayHello();
+var superman = new SuperHero("Superman", 20, 100);
+var hulk = new SuperHero("hulk", 30, 100);
+
+superman.sayHello();
+hulk.attack(superman);
+superman.attack(hulk);
+hulk.attack(superman);
+hulk.attack(superman);
+superman.attack(hulk);
