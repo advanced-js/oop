162f5c0a70d2bffc3fd61109303ca4d84da3af76
Juti Buican <bew.emc2@gmail.com>
2015-11-05 13:06:34 -0500

    reCreatedSuperHero
    Created two superHero
    Dr.Suresh attacked Claire but her level is up as she is set as the
    combatant2 which add her energy

--- js/super_hero.js
+++ js/super_hero.js
@@ -1,11 +1,34 @@
-// var SuperHero = ...
 "use strict";
+ var SuperHero = SuperHero || {} ;
 
-var SuperHero = {
-	name: 'Professor X',
-	greeting : function(){
-		return 'I am a ' + this.name;
-	}
-}
+ SuperHero = function(){
+ 	
+ 	this.name 		=  '';
+ 	this.mission 	=  '';
+ 	this.energy 	= 5;
+ 	this.fightLevel = 0;
+ 	this.attack 	= function(combatant1, combatant2){
+ 		combatant1.energy = combatant1.energy - combatant1.fightLevel; 
+ 		combatant2.energy ++;
+ 	};
+ };
 
-	
+
+
+var DrMohinderSuresh = new SuperHero();
+	DrMohinderSuresh.mame = 'Dr. Modinder Suresh';
+	DrMohinderSuresh.fightLevel = 1;
+	DrMohinderSuresh.energy = 5;
+console.log(DrMohinderSuresh);
+
+var ClaireBennet = new SuperHero();
+ 	ClaireBennet.name = ' Claire Bennet';
+ 	ClaireBennet.energy = 5;
+   
+   	DrMohinderSuresh.attack(DrMohinderSuresh, ClaireBennet);
+   	console.log('Current engey for '+ DrMohinderSuresh.name + ' is ' + DrMohinderSuresh.energy); 
+
+ 
+
+
+
--- js/tests.js
+++ js/tests.js
@@ -1,11 +1,14 @@
+
 QUnit.test("hello test", function(assert) {
   assert.strictEqual(1 + 1, 2, "One plus one is two");
 });
 
 // ADD TESTS HERE
 
-QUnit.test("SuperHero", function(){
+QUnit.test("SuperHero Object", function(assert){
 
-	ok(SuperHero.name, 'Super hero has a default name');
-
-})
+	ok(DrMohinderSuresh, 'Super hero is Dr. Mohinder Suresh');
+	ok (DrMohinderSuresh.energy, 'Dr. Modiner \'s current energy level ');
+	ok (ClaireBennet.energy, 'ClaireBennet energy level');
+	assert.strictEqual(ClaireBennet.energy, 6, "ClaireBennet should have energy of 6 after the attack");
+});
