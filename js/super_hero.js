b75b05b17ab610d94d57a24ce705b1c7c5d296e2
clenclen <dannyphu@gmail.com>
2016-02-18 17:15:11 -0500

    cup
    added cup method to see functions of prototype are shared across
    instances

--- js/super_hero.js
+++ js/super_hero.js
@@ -7,6 +7,11 @@ var SuperHero = function(name, power) {
 	}
 }
 
+SuperHero.prototype.cup = function() { 
+	return this.name + ' ' + this.power; 
+}
+
+
 SuperHero.prototype.sound = "Bang";
 SuperHero.prototype.makeSound = function(){
 	return " and says " + this.sound;
@@ -21,5 +26,10 @@ var superman = new SuperHero("Superman", "super strength")
 
 document.write(batman.getInfo() + batman.makeSound() + "<br />");
 document.write(superman.getInfo() + superman.makeSound() + "<br />");
-document.write(superman === batman + "<br />");
-document.write(superman.sound === batman.sound); 
+
+document.write(superman.cup() + "<br />"); 
+document.write((superman.cup === batman.cup) + "<br />");
+
+document.write((superman.name === batman.name) + "<br />");
+
+
