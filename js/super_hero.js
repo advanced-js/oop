cf43183dd39be896c6245d419c9c210765191425
clenclen <dannyphu@gmail.com>
2016-02-18 12:24:10 -0500

    V1
    added superhero and a sound method

--- js/super_hero.js
+++ js/super_hero.js
@@ -1,1 +1,25 @@
-// var SuperHero = ...
+
+var SuperHero = function(name, power) {
+	this.name = name; 
+	this.power = power; 
+	this.getInfo = function(){
+		return this.name + " has " + this.power;
+	}
+}
+
+SuperHero.prototype.sound = "Bang";
+SuperHero.prototype.makeSound = function(){
+	return " and says " + this.sound;
+}
+
+
+var batman = new SuperHero("Batman", "batarang"); 
+var superman = new SuperHero("Superman", "super strength")
+
+
+
+
+document.write(batman.getInfo() + batman.makeSound() + "<br />");
+document.write(superman.getInfo() + superman.makeSound() + "<br />");
+document.write(superman === batman + "<br />");
+document.write(superman.sound === batman.sound); 
