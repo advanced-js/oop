069e9066ddda669ced467330b8d88a507cceb36a
clenclen <dannyphu@gmail.com>
2016-02-25 16:47:30 -0500

    superhuman
    add class and two subclasses

--- js/super_hero.js
+++ js/super_hero.js
@@ -1,35 +1,57 @@
-
-var SuperHero = function(name, power) {
+var SuperHuman = function(name){
 	this.name = name; 
-	this.power = power; 
-	this.getInfo = function(){
-		return this.name + " has " + this.power;
-	}
+	this.health = 100; 	
 }
 
-SuperHero.prototype.cup = function() { 
-	return this.name + ' ' + this.power; 
-}
-
-
-SuperHero.prototype.sound = "Bang";
-SuperHero.prototype.makeSound = function(){
-	return " and says " + this.sound;
+SuperHuman.prototype.doublepunch = function(opp1, opp2) { 
+ 	this.health -= 3 
+ 	opp1.health -= 10;  
+ 	opp2.health -= 5; 
+ 	document.write(this.name + " has superpunched both " + opp1.name + " and " + opp2.name + "</br>")
 }
 
 
-var batman = new SuperHero("Batman", "batarang"); 
-var superman = new SuperHero("Superman", "super strength")
+SuperHuman.prototype.attack = function(opp) { 
+ 	opp.health -= 10;  
+ 	this.health -= 5; 
+ 	document.write(this.name + " just hit " + opp.name + "</br>")
+ 	document.write(this.name + " used 5 energy and has " + this.health + " left." + "</br>")
+ 	document.write(opp.name + " loses 10 energy and has " + opp.health + " left." + "</br>" + "</br>")
+}
+
+var SuperHero = function(name) {
+	SuperHuman.call(this)
+	this.type = "a hero"
+	this.name = name; 
+}
+
+SuperHero.prototype = Object.create(SuperHuman.prototype)
+
+SuperHero.prototype.heal = function(){
+	this.health += 8 
+	document.write(this.name + " has used healing power and now has " + this.health + " health </br>")
+}
+
+function SuperVillian(name) {
+	SuperHero.call(this)
+	this.type = "a villain"
+	this.name = name 	
+}
+
+SuperVillian.prototype = Object.create(SuperHuman.prototype)
 
 
+var batman = new SuperHero("batman"); 
+var superman = new SuperHero("superman")
+var joker = new SuperVillian("joker")
 
+batman.attack(joker)
+joker.attack(superman)
+superman.heal()
+joker.doublepunch(batman, superman)
 
-document.write(batman.getInfo() + batman.makeSound() + "<br />");
-document.write(superman.getInfo() + superman.makeSound() + "<br />");
+document.write( "</br>" + "health results:" + "</br>")
 
-document.write(superman.cup() + "<br />"); 
-document.write((superman.cup === batman.cup) + "<br />");
-
-document.write((superman.name === batman.name) + "<br />");
-
-
+document.write(batman.name + " is " + batman.type + " and has " + batman.health + " health left." + "</br>")
+document.write(superman.name +  " is " + superman.type + " and has " + superman.health + " health left." + "</br>")
+document.write(joker.name +  " is " + joker.type + " and has " + joker.health + " health left." + "</br>")
--- js/tests.js
+++ js/tests.js
@@ -2,4 +2,10 @@ QUnit.test("hello test", function(assert) {
   assert.strictEqual(1 + 1, 2, "One plus one is two");
 });
 
+
+QUnit.test("health matches", function(assert) {
+  assert.strictEqual(superman.health, superman.health , "health matches");
+});
+
+
 // ADD TESTS HERE
